11a77a14fa4d276e298f0d60b9540ed7bbd23b98
Hickey <jimmy@bff-000089-m.east78.local>
2013-01-23 11:15:30 -0500

    Initial Commit

---
+++ Password policy tool/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Password policy tool
+//
+//  Created by Hickey, Jimmy on 1/23/13.
+//  Copyright (c) 2013 Hickey, Jimmy. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ Password policy tool/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  Password policy tool
+//
+//  Created by Hickey, Jimmy on 1/23/13.
+//  Copyright (c) 2013 Hickey, Jimmy. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
